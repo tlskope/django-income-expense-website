f4d57e6d5f6d62ee99bb68cd493d980345d2b1a5
tlskope <128537840+tlskope@users.noreply.github.com>
2024-02-21 09:23:06 +1300

    first one

---
+++ expenseswebsite/expenseswebsite/testing.py
@@ -0,0 +1,5 @@
+import os
+print("Database Name:", os.environ.get('DB_NAME'))
+print("Database User:", os.environ.get('DB_USER'))
+print("Database Password:", os.environ.get('DB_USER_PASSWORD'))
+print("Database Host:", os.environ.get('DB_HOST'))
